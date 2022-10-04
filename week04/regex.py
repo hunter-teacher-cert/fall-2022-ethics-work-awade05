d07ef05ccdbd0ad3d76d41178dc86e9c1ffb221a
awade05 <51447771+awade05@users.noreply.github.com>
2022-10-03 21:32:51 -0400

    Update regex.py

--- week04/regex.py
+++ week04/regex.py
@@ -4,6 +4,9 @@ import re
 def find_name(line):
     pattern = r"([A-Za-z][-,A-Za-z. ']+[ ]*)+"
     result = re.findall(pattern,line)
+    
+    pattern = r'(?:Mr|Mrs|Ms|Miss|Madame|Mme|Dr|Prof|Sr)\.?\s+\w[A-Za-z]+\s+\w[A-Za-z]+'
+    result = result + re.findall(pattern,line)
     return result
 
 
